51456099a7db5d9f534976a5447be5f12e213815
Sherri Johns <sherri.johns@e-gineering.com>
2021-08-17 13:41:19 -0400

    display in table, add key to table

--- reacttraining/src/App.tsx
+++ reacttraining/src/App.tsx
@@ -1,30 +1,60 @@
 type Food = {
-    name: string;
-    quantity: number;
-}
+  id: number;
+  name: string;
+  quantity: number;
+  minquantity: number;
+  category: string;
+};
 
 const foods: Food[] = [
-{ name: "Carrot", quantity: 1},
-{ name: "Potato", quantity: 2},
-]
+  {
+    id: 1,
+    name: "Carrot",
+    quantity: 10,
+    minquantity: 5,
+    category: "Vegetable",
+  },
+  {
+    id: 2,
+    name: "Potato",
+    quantity: 20,
+    minquantity: 10,
+    category: "Vegetable",
+  },
+  {
+    id: 3,
+    name: "Rice Krispy",
+    quantity: 12,
+    minquantity: 2,
+    category: "Snack",
+  },
+];
 
 export function App() {
-
-    function renderFoods() {
-
-        // example of a concise arrow function
-        return foods.map(food => <li>{food.name}</li>)
-    };
-
-     return (
-     <>
-        <h1>E-gineering Pantry Manager</h1>
-        <ul>
-         { renderFoods() }
-        </ul>
+  return (
+    <>
+      <h1>E-gineering Pantry Manager</h1>
+      {/* example of a concise arrow function */}
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Category</th>
+            <th>Min Qty</th>
+          </tr>
+        </thead>
+        <tbody>
+          {foods.map((food, index) => (
+            <tr key={food.id}>
+              <td>{food.name}</td>
+              <td>{food.quantity}</td>
+              <td>{food.category}</td>
+              <td>{food.minquantity}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
-    );
+  );
 }
-
-
-
