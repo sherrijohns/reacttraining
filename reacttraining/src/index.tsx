49fbfda7c8a2cf49310d528bbba49708054ebc87
Sherri Johns <sherri.johns@e-gineering.com>
2021-08-18 15:40:51 -0400

    Add react router

--- reacttraining/src/index.tsx
+++ reacttraining/src/index.tsx
@@ -1,10 +1,24 @@
-import {render} from 'react-dom';
+import { render } from "react-dom";
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { About } from "./About";
+
+import { Nav } from "./Nav";
 
 // This is a named import (preferred by Cory)
 import { App } from "./App";
 
 //render(<Heading message="hello (from parms)" />, document.getElementById("root"));
 //render(<Heading>hello lunch!</Heading>, document.getElementById("root"));
-render (<App />, document.getElementById("root"));
-
+render(
+  <BrowserRouter>
+    <Nav />
+    <Route path="/about">
+      <About />
+    </Route>
+    <Route path="/" exact>
+      <App />
+    </Route>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
